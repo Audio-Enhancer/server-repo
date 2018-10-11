a54546733a2904cfeca32eae7b8198b476ba03c4
harlesbayu <bayuanggaraharles@gmail.com>
2018-10-11 18:47:27 +0700

    add server upload

--- routes/TheaudioRoutes.js
+++ routes/TheaudioRoutes.js
@@ -4,8 +4,19 @@ const express = require('express')
 const router = express.Router()
 const TheaudioController = require('../controllers/TheaudioController')
 const isLogin = require('../middlewares/isLogin')
+const media = require('../helpers/media')
 
 router.post('/',isLogin,TheaudioController.createAudio)
       .get('/', TheaudioController.getListOfAudio)
+
+router.post('/uploads/audio',
+      media.multer.single('audiofile'),
+      media.sendUploadToGCS,
+      (req, res) => {
+            res.status(200).json({
+            link: req.file.cloudStoragePublicUrl
+            })
+      }
+)
 
 module.exports = router
